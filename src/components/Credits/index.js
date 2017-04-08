424de9b1f22b6cdda9d5557919fdf9f0debe0f31
arshdkhn1 <arshdkhn1@gmail.com>
2017-04-09 00:46:30 +0530

    added update

---
+++ src/components/Credits/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './styles.sass';
+
+class Credits extends Component {
+  render() {
+    return (
+      <div className="credits">
+        <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+        <div>Icons made by <a href="http://www.flaticon.com/authors/zlatko-najdenovski" title="Zlatko Najdenovski">Zlatko Najdenovski</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+      </div>
+    );
+  }
+}
+
+export default Credits;
